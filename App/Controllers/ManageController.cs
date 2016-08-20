6a2d391e9457e18ae34cb47f9da54b04541faa04
clark <clark@DESKTOP-I1SM152>
2016-08-20 17:05:42 +0800

    控制面板页面

---
+++ App/Controllers/ManageController.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+
+using Langben.DAL;
+using Langben.App.Models;
+using Langben.BLL;
+using System.Drawing;
+using System;
+using System.Drawing.Imaging;
+using Common;
+using Langben.IBLL;
+using Models;
+namespace Langben.App.Controllers
+{
+    /// <summary>
+    /// 用户账户信息
+    /// </summary>
+    [HandleError]
+    public class ManageController : BaseController
+    {
+        /// <summary>
+        /// 控制面板页面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+
+            return View();
+
+        }
+    }
+         
+}
+
