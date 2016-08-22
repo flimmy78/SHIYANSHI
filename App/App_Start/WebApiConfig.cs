43380f99430aae990240992917418e7282b34b54
clark <clark@DESKTOP-I1SM152>
2016-08-22 14:15:44 +0800

    hone/index 问题解决

--- App/App_Start/WebApiConfig.cs
+++ App/App_Start/WebApiConfig.cs
@@ -7,6 +7,10 @@ namespace App
 {
     public static class WebApiConfig
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="config"></param>
         public static void Register(HttpConfiguration config)
         {
             config.MapHttpAttributeRoutes();
