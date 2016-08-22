6ce2121da2305380b9390fb22518749ebcbdc615
clark <clark@DESKTOP-I1SM152>
2016-08-22 22:43:44 +0800

    增加了方案管理

---
+++ App/Controllers/ManageTemplateController.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+
+using Langben.DAL;
+using Langben.App.Models;
+using Langben.BLL;
+using System.Drawing;
+using System;
+using System.Drawing.Imaging;
+using Common;
+using Langben.IBLL;
+using Models;
+namespace Langben.App.Controllers
+{
+    /// <summary>
+    /// 方案模板
+    /// </summary>
+    [HandleError]
+    public class ManageTemplateController : BaseController
+    {
+        /// <summary>
+        /// 控制面板页面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+
+            return View();
+
+        }
+    }
+         
+}
+
