47e6a036de8e31bef42aabc2e60cc702783114a4
clark <clark@DESKTOP-I1SM152>
2016-09-01 09:20:51 +0800

    器具登记

--- App/Controllers/ORDER_TASK_INFORMATIONController.cs
+++ App/Controllers/ORDER_TASK_INFORMATIONController.cs
@@ -71,7 +71,60 @@ namespace Langben.App.Controllers
         public ActionResult Save(ORDER_TASK_INFORMATION entity)
         {
 
-            return View();
+            Common.ClientResult.OrderTaskGong result = new Common.ClientResult.OrderTaskGong();
+            //如果是查询出来的委托单，又增加了一个器具，点击了发送实验室，怎么处理？
+         //   if (entity != null && ModelState.IsValid)
+            {
+                string currentPerson = GetCurrentPerson();
+                if (string.IsNullOrWhiteSpace(entity.ID))
+                { 
+                    entity.CREATETIME = DateTime.Now;
+                    entity.CREATEPERSON = currentPerson;
+                    entity.ID = Result.GetNewId();
+                    foreach (var item in entity.APPLIANCE_DETAIL_INFORMATION)
+                    {
+                        item.ID= Result.GetNewId();
+                        item.CREATETIME = DateTime.Now;
+                        item.CREATEPERSON = currentPerson;
+                    }
+
+                    string returnValue = string.Empty;
+                    if (m_BLL.Create(ref validationErrors, entity))
+                    {
+                        LogClassModels.WriteServiceLog(Suggestion.InsertSucceed + "，委托单信息的信息的Id为" + entity.ID, "委托单信息"
+                            );//写入日志 
+                        result.Code = Common.ClientCode.Succeed;
+                        result.Message = Suggestion.InsertSucceed;
+                        result.Id = entity.ID;
+                        return Json(result); //提示创建成功
+                    }
+                    else
+                    {
+                        if (validationErrors != null && validationErrors.Count > 0)
+                        {
+                            validationErrors.All(a =>
+                            {
+                                returnValue += a.ErrorMessage;
+                                return true;
+                            });
+                        }
+                        LogClassModels.WriteServiceLog(Suggestion.InsertFail + "，委托单信息的信息，" + returnValue, "委托单信息"
+                            );//写入日志                      
+                        result.Code = Common.ClientCode.Fail;
+                        result.Message = Suggestion.InsertFail + returnValue;
+                        return Json(result); //提示插入失败
+                    }
+                }
+                else
+                {
+
+                }
+            }
+
+            result.Code = Common.ClientCode.FindNull;
+            result.Message = Suggestion.InsertFail + "，请核对输入的数据的格式"; //提示输入的数据的格式不对 
+
+            return Json(result);
 
         }
         public ActionResult Createto(string id)
@@ -90,6 +143,17 @@ namespace Langben.App.Controllers
         {
             ViewBag.Id = id;
             return View();
+        }
+        IBLL.IORDER_TASK_INFORMATIONBLL m_BLL;
+
+        ValidationErrors validationErrors = new ValidationErrors();
+
+        public ORDER_TASK_INFORMATIONController()
+            : this(new ORDER_TASK_INFORMATIONBLL()) { }
+
+        public ORDER_TASK_INFORMATIONController(ORDER_TASK_INFORMATIONBLL bll)
+        {
+            m_BLL = bll;
         }
 
     }
---
+++ DAL/shiyanshi/APPLIANCE_DETAIL_INFORMATIONMeta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+namespace Langben.DAL
+{
+    public partial class APPLIANCE_DETAIL_INFORMATION
+    {
+
+        #region 自定义属性，即由数据实体扩展的实体
+
+        [Display(Name = "是否是新增的记录")]
+        public bool isNewRecord { get; set; }
+
+        #endregion
+
+    }
+   
+}
+
+
