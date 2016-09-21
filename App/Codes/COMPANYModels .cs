d31bc87196771f6b3ef13787f44272ba5adb038e
chengzhongxie <chengzhongxie@163.com>
2016-09-21 14:34:37 +0800

    器具登记送检单位和证书单位下拉框绑定

---
+++ App/Codes/COMPANYModels .cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Langben.BLL;
+using Langben.IBLL;
+namespace  Models
+{
+    public class COMPANYModels
+    {  
+        /// <summary>
+        /// 获取字段，首选默认，MyTexts做为value值
+        /// 谢承忠添加（器具登记页面单位绑定）
+        /// </summary>
+        /// <returns></returns>
+        public static SelectList GetCOMPANY(string CATEGORY)
+        {
+            if (string.IsNullOrWhiteSpace(CATEGORY))
+            {
+                List<SelectList> sl = new List<SelectList>();
+                return new SelectList(sl);
+            }
+            ICOMPANYHander compay = new SysCOMPANY();
+            return new SelectList(compay.GetCOMPANY(CATEGORY), "COMPANYNAME", "COMPANYNAME");
+
+        }
+    }
+}
+
---
+++ BLL/Framework/SysCOMPANY.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+using Langben.DAL;
+using Langben.IBLL;
+namespace Langben.BLL
+{
+    public class SysCOMPANY : IDisposable, ICOMPANYHander
+    {
+        protected SysEntities db = new SysEntities();
+     
+        /// <summary>
+        /// 获取下拉框的数据
+        /// </summary>
+        /// <param name="table">表名</param>
+        /// <param name="colum">列明</param>
+        /// <returns></returns>
+        public List<COMPANY> GetCOMPANY(string CATEGORY)
+        {
+
+            return (from m in db.COMPANY
+                    where m.CATEGORY == CATEGORY
+                    // orderby m.Sort
+                    select m).ToList();
+
+        }
+    
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+    }
+}
+
+
+
