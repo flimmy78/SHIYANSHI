50b39711d1ddc37800e40507b3f0781da4824ac8
chengzhongxie <chengzhongxie@163.com>
2016-10-09 17:54:01 +0800

    建立方案部分功能开发

---
+++ App/Models/METERING_STANDARD_DEVICEShow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Langben.App.Models
+{
+    public partial class METERING_STANDARD_DEVICEShow
+    {
+        public METERING_STANDARD_DEVICEShow()
+        {
+            METERING_STANDARD_DEVICE_CHECKShow = new List<METERING_STANDARD_DEVICE_CHECKShow>();
+            ALLOWABLE_ERRORShow = new List<ALLOWABLE_ERRORShow>();
+        }
+        public List<METERING_STANDARD_DEVICE_CHECKShow>METERING_STANDARD_DEVICE_CHECKShow { get; set; }
+        public List<ALLOWABLE_ERRORShow> ALLOWABLE_ERRORShow { get; set; }
+        public string ID { get; set; }
+        public string NAME { get; set; }
+        public string TEST_RANGE { get; set; }
+        public string FACTORY_NUM { get; set; }
+        public string CATEGORY { get; set; }
+        public string STATUS { get; set; }
+        public string UNDERTAKE_LABORATORYID { get; set; }
+        public Nullable<System.DateTime> CREATETIME { get; set; }
+        public string CREATEPERSON { get; set; }
+        public Nullable<System.DateTime> UPDATETIME { get; set; }
+        public string UPDATEPERSON { get; set; }
+    }
+}
---
+++ App/Models/METERING_STANDARD_DEVICE_CHECKShow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Langben.App.Models
+{
+    public partial class METERING_STANDARD_DEVICE_CHECKShow
+    {
+        public METERING_STANDARD_DEVICE_CHECKShow()
+        {
+
+        }
+        public string ID { get; set; }
+        public string CERTIFICATE_NUM { get; set; }
+        public Nullable<System.DateTime> CHECK_DATE { get; set; }
+        public Nullable<System.DateTime> VALID_TO { get; set; }
+        public string METERING_STANDARD_DEVICEID { get; set; }
+        public Nullable<System.DateTime> CREATETIME { get; set; }
+        public string CREATEPERSON { get; set; }
+        public Nullable<System.DateTime> UPDATETIME { get; set; }
+        public string UPDATEPERSON { get; set; }
+    }
+}
