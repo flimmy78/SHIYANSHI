38dfe608888d27ad1578653b51636c640bde40b6
chengzhongxie <chengzhongxie@163.com>
2016-11-23 10:07:47 +0800

    方法删除

--- App/Controllers/PROJECTTEMPLETController.cs
+++ App/Controllers/PROJECTTEMPLETController.cs
@@ -203,18 +203,7 @@ namespace Langben.App.Controllers
             ViewBag.canshu = id;
             return Detail(RULEID, SCHEMEID, PREPARE_SCHEMEID);
         }
-        /// <summary>
-        /// 直流电压（电流）测量-非正负极性-绝对误差
-        /// </summary> 
-        /// <param name="RULEID">检测项目ID</param>
-        /// <param name="SCHEMEID">方案ID</param>
-        /// <returns></returns>
-        public ActionResult ZhiLiuDianLiuDianYaFeiZhengFuJueDuiWuCha(string id = "电压输出", string RULEID = "315-1983_2_1", string SCHEMEID = "", string PREPARE_SCHEMEID = "")
-        {
-            //电压输出
-            ViewBag.canshu =id;
-            return Detail(RULEID, SCHEMEID, PREPARE_SCHEMEID);
-        }
+
         /// <summary>
         /// 直流电压（电流）测量-正负极性-绝对误差
         /// </summary> 
