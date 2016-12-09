24cc764485694e8d44d34805dd9f0fe9d33a9fd3
chengzhongxie <chengzhongxie@163.com>
2016-12-09 22:29:58 +0800

    表格添加

---
+++ DAL/STANDARDCHOICEMeta.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+namespace Langben.DAL
+{
+    [MetadataType(typeof(STANDARDCHOICEMetadata))]//使用STANDARDCHOICEMetadata对STANDARDCHOICE进行数据验证
+    public partial class STANDARDCHOICE 
+    {
+      
+        #region 自定义属性，即由数据实体扩展的实体
+        
+        [Display(Name = "预备方案")]
+        public string PREPARE_SCHEMEIDOld { get; set; }
+        
+        #endregion
+
+    }
+    public partial class STANDARDCHOICEMetadata
+    {
+			[ScaffoldColumn(false)]
+			[Display(Name = "主键", Order = 1)]
+			public object ID { get; set; }
+
+			[ScaffoldColumn(true)]
+			[Display(Name = "组别", Order = 2)]
+			public object GROUPS { get; set; }
+
+			[ScaffoldColumn(true)]
+			[Display(Name = "类型", Order = 3)]
+			public object TYPE { get; set; }
+
+			[ScaffoldColumn(true)]
+			[Display(Name = "创建时间", Order = 4)]
+			[DataType(System.ComponentModel.DataAnnotations.DataType.DateTime,ErrorMessage="时间格式不正确")]
+			public DateTime? CREATETIME { get; set; }
+
+			[ScaffoldColumn(true)]
+			[Display(Name = "创建者", Order = 5)]
+			public object CREATEPERSON { get; set; }
+
+			[ScaffoldColumn(true)]
+			[Display(Name = "修改时间", Order = 6)]
+			[DataType(System.ComponentModel.DataAnnotations.DataType.DateTime,ErrorMessage="时间格式不正确")]
+			public DateTime? UPDATETIME { get; set; }
+
+			[ScaffoldColumn(true)]
+			[Display(Name = "修改人", Order = 7)]
+			public object UPDATEPERSON { get; set; }
+
+			[ScaffoldColumn(true)]
+			[Display(Name = "预备方案", Order = 8)]
+			public object PREPARE_SCHEMEID { get; set; }
+
+
+    }
+}
+ 
+
