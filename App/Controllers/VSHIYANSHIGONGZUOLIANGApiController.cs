d3bc3ebb546d1fe7e63d4c985abe011aeec5afaa
chengzhongxie <chengzhongxie@163.com>
2016-12-14 15:03:36 +0800

    添加视图

---
+++ App/Controllers/VSHIYANSHIGONGZUOLIANGApiController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Text;
+using System.EnterpriseServices;
+using System.Configuration;
+using Models;
+using Common;
+using Langben.DAL;
+using Langben.BLL;
+using System.Web.Http;
+using Langben.App.Models;
+
+namespace Langben.App.Controllers
+{
+    /// <summary>
+    /// 实验室别工作量统计分析
+    /// </summary>
+    public class VSHIYANSHIGONGZUOLIANGApiController : BaseApiController
+    {
+        /// <summary>
+        /// 异步加载数据
+        /// </summary>
+        /// <param name="getParam"></param>
+        /// <returns></returns>
+        public Common.ClientResult.DataResult PostData([FromBody]GetDataParam getParam)
+        {
+            int total = 0;
+            List<VSHIYANSHIGONGZUOLIANG> queryData = m_BLL.GetByParam(getParam.id, getParam.page, getParam.rows, getParam.order, getParam.sort, getParam.search, ref total);
+            var data = new Common.ClientResult.DataResult
+            {
+                total = total,
+                rows = queryData.Select(s => new
+                {
+                    ID = s.ID
+					,WEITUODAN = s.WEITUODAN
+					,JIANDINGWANCHENG = s.JIANDINGWANCHENG
+					,SHEBEIGUZHANG = s.SHEBEIGUZHANG
+					,PIZHUNTONGGUO = s.PIZHUNTONGGUO
+					,HEGE = s.HEGE
+					,BUHEGE = s.BUHEGE
+					,CHAOQI = s.CHAOQI
+					
+
+                })
+            };
+            return data;
+        }
+
+        /// <summary>
+        /// 根据ID获取数据模型
+        /// </summary>
+        /// <param name="id">编号</param>
+        /// <returns></returns>
+        public VSHIYANSHIGONGZUOLIANG Get(^string^ id)
+        {
+            VSHIYANSHIGONGZUOLIANG item = m_BLL.GetById(id);
+            return item;
+        }
+  
+
+        IBLL.IVSHIYANSHIGONGZUOLIANGBLL m_BLL;
+
+        ValidationErrors validationErrors = new ValidationErrors();
+
+        public VSHIYANSHIGONGZUOLIANGApiController()
+            : this(new VSHIYANSHIGONGZUOLIANGBLL()) { }
+
+        public VSHIYANSHIGONGZUOLIANGApiController(VSHIYANSHIGONGZUOLIANGBLL bll)
+        {
+            m_BLL = bll;
+        }
+        
+    }
+}
+
+
---
+++ IBLL/IVSHIYANSHIGONGZUOLIANGBLL.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Common;
+using Langben.DAL;
+using System.ServiceModel;
+
+namespace Langben.IBLL
+{
+    /// <summary>
+    /// 实验室别工作量统计分析 接口
+    /// </summary>
+    [ServiceContract(Namespace = "www.langben.com")]
+    public interface IVSHIYANSHIGONGZUOLIANGBLL
+    {
+        /// <summary>
+        /// 查询的数据
+        /// </summary>
+        /// <param name="id">额外的参数</param>
+        /// <param name="page">页码</param>
+        /// <param name="rows">每页显示的行数</param>
+        /// <param name="order">排序字段</param>
+        /// <param name="sort">升序asc（默认）还是降序desc</param>
+        /// <param name="search">查询条件</param>
+        /// <param name="total">结果集的总数</param>
+        /// <returns>结果集</returns>
+        [OperationContract]
+        List<VSHIYANSHIGONGZUOLIANG> GetByParam(string id, int page, int rows, string order, string sort, string search, ref int total);
+        /// <summary>
+        /// 获取所有
+        /// </summary>
+        /// <returns></returns>
+        [OperationContract]
+        System.Collections.Generic.List<VSHIYANSHIGONGZUOLIANG> GetAll();
+        /// <summary>
+        /// 根据主键，查看详细信息
+        /// </summary>
+        /// <param name="id">根据主键</param>
+        /// <returns></returns>
+        [OperationContract]
+        VSHIYANSHIGONGZUOLIANG GetById(^string^ id);    
+ 
+    }
+}
+
