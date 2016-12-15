b9c324bda7086308a7d21c3be96b4083487e96df
chengzhongxie <chengzhongxie@163.com>
2016-12-15 17:14:53 +0800

    报表

--- DAL/VSHIYANSHIGONGZUOLIANGRepository.cs
+++ DAL/VSHIYANSHIGONGZUOLIANGRepository.cs
@@ -21,54 +21,28 @@ namespace Langben.DAL
         /// <returns></returns>      
         public List<SHIYANSHIGONGZUO_Result> GetData(SysEntities db, string order, string sort, string search, params object[] listQuery)
         {
-            string where = string.Empty;
-            int flagWhere = 0;
-
             Nullable<System.DateTime> sTARTDATE = null; Nullable<System.DateTime> eNDDATE = null; string dANWEI = string.Empty;
        
             Dictionary<string, string> queryDic = ValueConvert.StringToDictionary(search.GetString());
             if (queryDic != null && queryDic.Count > 0)
             {
                 foreach (var item in queryDic)
-                {
-                    if (flagWhere != 0)
-                    {
-                        where += " and ";
-                    }
-                    flagWhere++;
-                                    
+                {                                                     
                     if (!string.IsNullOrWhiteSpace(item.Key) && !string.IsNullOrWhiteSpace(item.Value) && item.Key.Contains(Start_Time)) //开始时间
                     {
-                        where += "it.[" + item.Key.Remove(item.Key.IndexOf(Start_Time)) + "] >=  CAST('" + item.Value + "' as   System.DateTime)";
+                        sTARTDATE =Convert.ToDateTime(item.Value);
                         continue;
                     }
                     if (!string.IsNullOrWhiteSpace(item.Key) && !string.IsNullOrWhiteSpace(item.Value) && item.Key.Contains(End_Time)) //结束时间+1
                     {
-                        where += "it.[" + item.Key.Remove(item.Key.IndexOf(End_Time)) + "] <  CAST('" + Convert.ToDateTime(item.Value).AddDays(1) + "' as   System.DateTime)";
+                        eNDDATE = Convert.ToDateTime(item.Value).AddDays(1);
                         continue;
                     }
-                    if (!string.IsNullOrWhiteSpace(item.Key) && !string.IsNullOrWhiteSpace(item.Value) && item.Key.Contains(Start_Int)) //开始数值
+                    if (!string.IsNullOrWhiteSpace(item.Key) && !string.IsNullOrWhiteSpace(item.Value) && item.Key== "SHOULIDANWEI") //
                     {
-                        where += "it.[" + item.Key.Remove(item.Key.IndexOf(Start_Int)) + "] >= " + item.Value.GetInt();
+                        dANWEI = item.Value;
                         continue;
                     }
-                    if (!string.IsNullOrWhiteSpace(item.Key) && !string.IsNullOrWhiteSpace(item.Value) && item.Key.Contains(End_Int)) //结束数值
-                    {
-                        where += "it.[" + item.Key.Remove(item.Key.IndexOf(End_Int)) + "] <= " + item.Value.GetInt();
-                        continue;
-                    }
-     
-                    if (!string.IsNullOrWhiteSpace(item.Key) && !string.IsNullOrWhiteSpace(item.Value) && item.Key.Contains(DDL_Int)) //精确查询数值
-                    {
-                        where += "it.[" + item.Key.Remove(item.Key.IndexOf(DDL_Int)) + "] =" + item.Value;
-                        continue;
-                    }
-                    if (!string.IsNullOrWhiteSpace(item.Key) && !string.IsNullOrWhiteSpace(item.Value) && item.Key.Contains(DDL_String)) //精确查询字符串
-                    {
-                        where += "it.[" + item.Key.Remove(item.Key.IndexOf(DDL_String)) + "] = '" + item.Value + "'";
-                        continue;
-                    }
-                    where += "it.[" + item.Key + "] like '%" + item.Value + "%'";//模糊查询
                 }
             }
 
@@ -87,7 +61,7 @@ namespace Langben.DAL
             }
 
 
-            var data = db.SHIYANSHIGONGZUO(System.DateTime.Now.AddYears(-1), System.DateTime.Now.AddYears(1), "*").ToList();
+            var data = db.SHIYANSHIGONGZUO(System.DateTime.Now.AddYears(-1), System.DateTime.Now.AddYears(1), dANWEI).ToList();
 
 
 
