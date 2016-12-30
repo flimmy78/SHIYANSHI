ac4fcd36fc45ccf44fb8ff0cb598d5b9eede0daf
chengzhongxie <chengzhongxie@163.com>
2016-12-30 15:49:08 +0800

    提取时间

--- App/Models/APPLIANCE_DETAIL_INFORMATIONShow.cs
+++ App/Models/APPLIANCE_DETAIL_INFORMATIONShow.cs
@@ -44,6 +44,7 @@ namespace Langben.App.Models
         public string REPORTTORECEVESTATE { get; set; }
         public APPLIANCE_LABORATORYShow APPLIANCE_LABORATORYShow { get; set; }
         public string ORDER_STATUS_NAME { get; set; }//记入退回实验室
+        public string TIQURIQI { get; set; }//提前日期
         //  public virtual ORDER_TASK_INFORMATIONShow ORDER_TASK_INFORMATION { get; set; }
     }
 }
