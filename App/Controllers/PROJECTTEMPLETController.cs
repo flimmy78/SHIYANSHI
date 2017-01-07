427c458321f4fb749291a9a05c45d284e12cca00
clark <ben@langben.com>
2017-01-07 16:16:47 +0800

    js

--- App/Controllers/PROJECTTEMPLETController.cs
+++ App/Controllers/PROJECTTEMPLETController.cs
@@ -19,13 +19,26 @@ namespace Langben.App.Controllers
     public class PROJECTTEMPLETController : BaseController
     {//
      /// <summary>
-     ///位置影响
+     ///二次负荷导纳阻抗误差
      /// </summary>
      /// <param name="id">canshu</param>
      /// <param name="RULEID">规程id</param>
      /// <param name="SCHEMEID">方案id</param>
      /// <param name="PREPARE_SCHEMEID">预备方案id</param>
      /// <returns></returns>
+        public ActionResult ErCiFuHeDaoNaZuKangWuCha(string RULEID = "126-1995_2_6_1", string SCHEMEID = "", string PREPARE_SCHEMEID = "")
+        {
+
+            return Detail(RULEID, SCHEMEID, PREPARE_SCHEMEID);
+        }
+        /// <summary>
+        ///位置影响
+        /// </summary>
+        /// <param name="id">canshu</param>
+        /// <param name="RULEID">规程id</param>
+        /// <param name="SCHEMEID">方案id</param>
+        /// <param name="PREPARE_SCHEMEID">预备方案id</param>
+        /// <returns></returns>
         public ActionResult WeiZhiYingXiang(string RULEID = "126-1995_2_6_1", string SCHEMEID = "", string PREPARE_SCHEMEID = "")
         {
 
