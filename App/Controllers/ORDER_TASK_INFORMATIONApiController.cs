614aff4eb744a70f2ee685aa839e4ee973dd76c7
clark <ben@langben.com>
2017-01-22 12:58:32 +0800

    js

--- App/Controllers/ORDER_TASK_INFORMATIONApiController.cs
+++ App/Controllers/ORDER_TASK_INFORMATIONApiController.cs
@@ -127,7 +127,7 @@ namespace Langben.App.Controllers
                         ORDER_STATUS_NAME = item2.UNDERTAKE_LABORATORYID + item2.ORDER_STATUS;
                       
                     }
-                    UNDERTAKE_LABORATORYID = item2.UNDERTAKE_LABORATORYID;
+                    UNDERTAKE_LABORATORYID +=","+ item2.UNDERTAKE_LABORATORYID;
                    
                 }
                 if (list.Count>1)
