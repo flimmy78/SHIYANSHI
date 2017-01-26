6adb1f020d251acf96b69b33a7f587686602790e
clark <ben@langben.com>
2017-01-26 12:15:56 +0800

    READVALUE

--- App/Scripts/JScript-JianCeXiang-Common.js
+++ App/Scripts/JScript-JianCeXiang-Common.js
@@ -6,7 +6,7 @@ var DanWeiDDLHtmlArray = Array;
 DanWeiDDLHtmlArray = [
            {
                Code: 'ZhiHouChaoQian',
-               Remark: '额定工作电流',
+               Remark: '滞后',
                Value: "<select class=\"my-combobox\" name=\"ZhiHouChaoQian\" style=\"width:50px; \">" +
                        "<option value=\"滞后\">滞后</option> " +
                        "<option value=\"超前\">超前</option>" +
@@ -2831,7 +2831,7 @@ RuleAttributeArray = [{
                 //检测项属性单位下拉框选项单位类型1|检测项属性单位名称1,检测项属性单位名称2;单位类型2|检测项属性单位名称1,检测项属性单位名称2
                 //DianLiu:电流单位；DianYa:电压单位   
                 //DianLiu|RANGE_UNIT:量程,OUTPUT_VALUE_UNIT:输出示值,ACTUAL_OUTPUT_VALUE_UNIT：输出实际值;DianYa|READ_VALUE_UNIT:读数值',
-                DanWeiHtmlDDL: '',
+                DanWeiHtmlDDL: 'ZhiHouChaoQian|READVALUE',
                 //READ_VALUE:读数值,ACTUAL_OUTPUT_VALUE:输出实际值,RELATIVE_ERROR:相对误差,UNCERTAINTY_DEGREE:不确定度,REMARK:注,CONCLUSION:结论
                 BuBaoCunShuJu: '',
                 //添加量程自动计算赋值列,检测项属性单位名称1,检测项属性单位名称2,
