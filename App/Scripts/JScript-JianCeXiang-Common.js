61abfdda34252d68d7cc6ca712add2ff87424170
clark <ben@langben.com>
2017-02-02 21:31:06 +0800

    http://localhost:55977/PROJECTTEMPLET/JiBenWuChaJianDing?RULEID=622-1997_3&SCHEMEID=1702021553020305147c1f7745ff0

--- App/Scripts/JScript-JianCeXiang-Common.js
+++ App/Scripts/JScript-JianCeXiang-Common.js
@@ -3151,7 +3151,7 @@ RuleAttributeArray = [{
                 //检测项属性单位下拉框选项单位类型1|检测项属性单位名称1,检测项属性单位名称2;单位类型2|检测项属性单位名称1,检测项属性单位名称2
                 //DianLiu:电流单位；DianYa:电压单位   
                 //DianLiu|RANGE_UNIT:量程,OUTPUT_VALUE_UNIT:输出示值,ACTUAL_OUTPUT_VALUE_UNIT：输出实际值;DianYa|READ_VALUE_UNIT:读数值',
-                DanWeiHtmlDDL: 'DianYa|RANGE,OUTPUTVALUE,ACTUALVALUE,SHIJISHUCHUZHI',
+                DanWeiHtmlDDL: 'DianYa|RANGE;OM|OUTPUTVALUE,ACTUALVALUE,SHIJISHUCHUZHI',
                 //READ_VALUE:读数值,ACTUAL_OUTPUT_VALUE:输出实际值,RELATIVE_ERROR:相对误差,UNCERTAINTY_DEGREE:不确定度,REMARK:注,CONCLUSION:结论
                 BuBaoCunShuJu: '',
                 //添加量程自动计算赋值列,检测项属性单位名称1,检测项属性单位名称2,
