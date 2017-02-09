faf1639a831e16b469c39a903e749862826b70c3
mc3231129 <mc3231129@sina.com>
2017-02-09 12:22:57 +0800

    修改  基本误差和升降变差440

--- App/Scripts/JScript-JianCeXiang-Common.js
+++ App/Scripts/JScript-JianCeXiang-Common.js
@@ -4,7 +4,24 @@ var JiSuanBuQueDingDu_MeiHang_Html = " <a href=\"javascript:void(0)\" class=\"ea
 //检测项目单位
 var DanWeiDDLHtmlArray = Array;
 DanWeiDDLHtmlArray = [
-        
+          {
+              Code: 'DYHG',
+              Remark: '电压互感器测量回路说明',
+              Value: "<select class=\"my-combobox\" name=\"DYHG\" style=\"width:50px; \">" +
+                      "<option value=\"ΔU/U\">ΔU/U</option> " +
+                      "<option value=\"ΔI/I\">ΔI/I</option>" +
+                     "</select>"
+          }, {
+              Code: 'DLHG',
+              Remark: '电压互感器测量回路说明',
+              Value: "<select class=\"my-combobox\" name=\"DLHG\" style=\"width:50px; \">" +
+
+                      "<option value=\"U=100V\">U=100V</option>" +
+                      "<option value=\"U=100/√3V\">U=100/√3V</option> " +
+                      "<option value=\"I=1A\">I=1A</option> " +
+                      "<option value=\"I=5A\">I=5A</option>  " +
+                     "</select>"
+          },
         {
             Code: 'DYDLW',
             Remark: '电压单位',
@@ -508,7 +525,8 @@ DanWeiDDLHtmlArray = [
 ]
 //检测项控制属性
 var RuleAttributeArray = new Array;
-RuleAttributeArray = [{
+RuleAttributeArray = [
+    {//
     RuleID: 'test',//检测项编号
     Remark: '直流电流输出-相对误差',//检测项说明
     Attributes:
@@ -536,7 +554,36 @@ RuleAttributeArray = [{
             JiSuanBuQueDingDu_DiBu: 'N'
 
         }]
-}
+    }, {
+        RuleID: '169-2010_6_2',//检测项编号
+        Remark: '电压互感器测量回路',//检测项说明
+        Attributes:
+        [{
+            //添加通道按钮是否显示show:显示，hidden:不显示
+            DuoTongDao: 'show',
+            //检测项单位联动下拉框联动:变更项名称1|联动相名称1,联动相名称2;变更项名称2|联动相名称1,联动相名称2
+            //一个量程下的单位与对应的输出示值和输出实际值的单位联动，更改量程的单位输出示值和输出实际值单位自动变更
+            LianDongDanWeiDDL: '',
+            //检测项属性单位下拉框选项单位类型1|检测项属性单位名称1,检测项属性单位名称2;单位类型2|检测项属性单位名称1,检测项属性单位名称2
+            //DianLiu:电流单位；DianYa:电压单位   
+            //DianLiu|RANGE_UNIT:量程,OUTPUT_VALUE_UNIT:输出示值,ACTUAL_OUTPUT_VALUE_UNIT：输出实际值;DianYa|READ_VALUE_UNIT:读数值',
+            DanWeiHtmlDDL: '',
+            //READ_VALUE:读数值,ACTUAL_OUTPUT_VALUE:输出实际值,RELATIVE_ERROR:相对误差,UNCERTAINTY_DEGREE:不确定度,REMARK:注,CONCLUSION:结论
+            BuBaoCunShuJu: '',
+            //添加量程自动计算赋值列,检测项属性单位名称1,检测项属性单位名称2,
+            //OUTPUT_VALUE：输出示值
+            CalculateForAddLianCheng: '',
+            //不确定度
+            //检测项属性名称后需要加自动或者按钮不确定计算
+            //自动计算不确定(Z:检测项属性名称|显示值属性名称或者输出实际值属性名称;量程属性名称;K属性名称;选用电阻属性名称;输出示值属性名称或者标准值属性名称;频率属性名称)
+            //自动计算不确定度中的显示值属性名称或者输出实际值属性名称;量程属性名称;K属性名称;选用电阻属性名称;输出示值属性名称或者标准值属性名称;频率属性名称，如果对应的属性可以为空，但顺序不能变并且个数不能变
+            //例如：显示值属性名称;量程属性名称;K属性名称;;;;
+            //按钮计算不确定(A:检测项属性名称)
+            JiSuanBuQueDingDu: '',
+            //是否有底部计算不确定度按钮,(D:动态可添加行、G:固定两行,如果没有可不设置)                
+            JiSuanBuQueDingDu_DiBu: 'G'
+        }]
+        }
     ,
     {
         RuleID: '38-1987_2_1',//检测项编号
@@ -596,7 +643,7 @@ RuleAttributeArray = [{
                 //按钮计算不确定(A:检测项属性名称)
                 JiSuanBuQueDingDu: 'Z:UNCERTAINTYDEGREE|SHIJISHUCHUZHI;RANGE;K;OUTPUTVALUE;READVALUE;;',
                 //是否有底部计算不确定度按钮,(D:动态可添加行、G:固定两行,如果没有可不设置)                
-                JiSuanBuQueDingDu_DiBu: 'N'
+                JiSuanBuQueDingDu_DiBu: 'G'
             }]
     }
         ,
