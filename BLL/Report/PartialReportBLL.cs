8730e6950ea555f01df18b4fc5d12f1018f19d24
clark <ben@langben.com>
2017-02-26 09:39:35 +0800

    解析xml

--- BLL/Report/PartialReportBLL.cs
+++ BLL/Report/PartialReportBLL.cs
@@ -299,10 +299,14 @@ namespace Langben.Report
                         {
                             doc.LoadHtml(iEntity.HTMLVALUE);
                             string data = AnalyticHTML.Getinput(doc);
+                            if (iEntity.RULEID== "1052-2009_6")
+                            {
+
+                            }
                             string data2 = AnalyticHTML.GetinputHead(doc);
                             if (!string.IsNullOrWhiteSpace(data2))
                             {
-                                errors += "<br/><br/>thead标签中，name是不能相同的" + iEntity.ID+ iEntity.RULE.ID + iEntity.RULE.NAMEOTHER + data2;
+                                errors += "<br/><br/>thead标签中，name是不能相同的" + iEntity.ID + iEntity.RULE.ID + iEntity.RULE.NAMEOTHER + data2;
                             }
                             if (!string.IsNullOrWhiteSpace(data))
                             {
