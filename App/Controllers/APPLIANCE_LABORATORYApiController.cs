4f3c8da3159eda4b7c41932e9fdffae503344e42
chengzhongxie <chengzhongxie@163.com>
2017-03-24 15:15:17 +0800

    退回

--- App/Controllers/APPLIANCE_LABORATORYApiController.cs
+++ App/Controllers/APPLIANCE_LABORATORYApiController.cs
@@ -419,10 +419,11 @@ namespace Langben.App.Controllers
                         entity.APPLIANCE_DETAIL_INFORMATION.STORAGEINSTRUCTIONS = entity.STORAGEINSTRUCTIONS.ToString();//入库说明
                         if (m_BLL2.EditField(ref validationErrors, entity.APPLIANCE_DETAIL_INFORMATION))//修改器具明细表中的入库说明
                         {
+                            var aryTwo = appory.FirstOrDefault();
+                            string ids = aryTwo.UNDERTAKE_LABORATORYID;
                             if (appory.Remove(aryOne))
-                            {
-                                var aryTwo = appory.FirstOrDefault();
-                                result.Message = "请通知" + aryTwo.UNDERTAKE_LABORATORYID + "该器具不能检测";
+                            {                              
+                                result.Message = "请通知" + ids + "该器具不能检测";
                             }
                             else
                             {
