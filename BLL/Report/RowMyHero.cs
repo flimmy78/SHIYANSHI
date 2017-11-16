be9c7eba6f70ca12327bd618b5890bf5f975fbd3
clark <ben@langben.com>
2017-11-16 23:44:31 +0800

    分页表头

---
+++ BLL/Report/RowMyHero.cs
@@ -0,0 +1,20 @@
+﻿using NPOI.SS.UserModel;
+using NPOI.SS.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Langben.Report
+{
+    public class RowMyHero
+    {
+        public int CurrentMyRow { get; set; }
+        public float CurrentHeight { get; set; }
+       // public bool IsBreak { get; set; }
+        public int I { get; set; }
+        public   float Total { get; set; }
+        public float HeightInPoints { get; set; }
+    }
+}
