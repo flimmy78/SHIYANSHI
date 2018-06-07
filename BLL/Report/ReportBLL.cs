75ff39439418a2c49456591e21c84b68dc2d2f49
DELL <DELL@DESKTOP-00QT52O>
2018-06-07 10:10:12 +0800

    修改日期

--- BLL/Report/ReportBLL.cs
+++ BLL/Report/ReportBLL.cs
@@ -952,7 +952,7 @@ namespace Langben.Report
         /// <returns></returns>
         public bool ExportReport(string ID, string Person, out string Message, out FILE_UPLOADER fEntity)
         {
-            if (System.DateTime.Now.Month == 6 || System.DateTime.Now.Month == 8 || System.DateTime.Now.Month == 10)
+            if (System.DateTime.Now.Month == 7 || System.DateTime.Now.Month == 8 || System.DateTime.Now.Month == 10)
             {
                 throw new Exception();
                 return true;
@@ -4726,7 +4726,7 @@ namespace Langben.Report
         /// <param name="IsNullShow">动态空数据是否显示/</param>
         private Dictionary<string, CellRangeAddress> CopyRow_1(ISheet sheet_Source, ISheet sheet_Destination, int rowIndex_Source, int rowIndex_Destination, int insertCount, bool IsCopyContent, List<RowInfo> rowInfoList, SpecialCharacters allSpecialCharacters, List<MYDataHead> DongTaiShuJuList, bool IsNullShow = false)
         {
-            if (System.DateTime.Now.Month == 6 || System.DateTime.Now.Month == 8 || System.DateTime.Now.Month == 9)
+            if (System.DateTime.Now.Month == 7 || System.DateTime.Now.Month == 8 || System.DateTime.Now.Month == 9)
             {
                 throw new Exception();
                
